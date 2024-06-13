db2f0a3efb2d6815f48583b0d18c14ae6bb12bde
Javier Alarcon <javieralarconbusiness@outlook.com>
2024-06-12 18:51:35 -0700

    Added some new comments and export css file

--- App/static/js/plot1.js
+++ App/static/js/plot1.js
@@ -1,7 +1,9 @@
 // Fetch Alcaldia data and populate the dropdown
 d3.json('/alcaldias').then(function(alcaldiaData) {
+    // We Select the alcaldiaSelect Select id
     const select = document.getElementById('alcaldiaSelect');
     alcaldiaData.forEach(alcaldia => {
+        // We add and html option element and populate with the alcaldia name fetch from /alcaldias json
         const option = document.createElement('option');
         option.value = alcaldia.nombre;
         option.text = alcaldia.nombre;
@@ -10,21 +12,26 @@ d3.json('/alcaldias').then(function(alcaldiaData) {
 
     // Fetch incident data and initialize the chart
     d3.json('/incidentes_plot1').then(function(data) {
+        // Array that contains the incident_type
         const types = Array.from(new Set(data.map(d => d.incident_type)));
 
+        // function takes alcaldia name as the entry value
         function updateChart(alcaldia) {
+            // filter the data that contains only the same as the entry value of the function
             const filteredData = data.filter(d => d.alcaldia === alcaldia);
+            // counts contains the number of counts and the types contains unique accident type
             const counts = types.map(type => {
                 const incident = filteredData.find(d => d.incident_type === type);
+                // uf there is no incident fount then the incident value is 0
                 return incident ? incident.incident_count : 0;
             });
-
+            // makeing the trace and type of graph
             const trace = {
                 x: types,
                 y: counts,
                 type: 'bar'
             };
-
+            // layout
             const layout = {
                 title: `Incidents by Type in ${alcaldia}`,
                 xaxis: {
@@ -49,12 +56,10 @@ d3.json('/alcaldias').then(function(alcaldiaData) {
         select.addEventListener('change', function() {
             updateChart(this.value);
         });
-    }).catch(function(error) {
-        console.log('Error fetching incident data:', error);
-    });
-}).catch(function(error) {
-    console.log('Error fetching alcaldia data:', error);
-});
+
+    }).catch(function(error) {console.log('Error fetching incident data:', error);});
+
+}).catch(function(error) {console.log('Error fetching alcaldia data:', error);});
 
 
 
